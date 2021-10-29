7e911186e6d70145887d74ca332b41ffe3e94c80
rajvrinda <rajk.dev1010@gmail.com>
2021-10-29 17:24:35 +0530

    Initial Commit

---
+++ secv/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  secv
+//
+//  Created by RAJKUMAR on 29/10/21.
+//  Copyright © 2021 RAJKUMAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ secvTests/secvTests.swift
@@ -0,0 +1,34 @@
+//
+//  secvTests.swift
+//  secvTests
+//
+//  Created by RAJKUMAR on 29/10/21.
+//  Copyright © 2021 RAJKUMAR. All rights reserved.
+//
+
+import XCTest
+@testable import secv
+
+class secvTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
